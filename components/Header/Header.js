c7b26e019f75f22ed71b7b8ee30ed673ffab0267
mthomsen77 <michael2004thomsen@hotmail.com>
2021-12-19 00:10:07 +0100

    minor responsive styles

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,10 +5,13 @@ import Menu from "./Menu";
 const Header = ({ menu }) => {
   return (
     <div className={styles.header}>
-      <div>
+      <div className={styles.desk}>
         <Image src={menu.data.logo.url} width={120} height={50} />
         <Menu items={menu.data.menu_links} />
         <p>Go to App</p>
+      </div>
+      <div className={styles.mobile}>
+       mobile
       </div>
     </div>
   );
--- slices/MeetTheTeam/index.js
+++ slices/MeetTheTeam/index.js
@@ -4,44 +4,51 @@ import Image from "next/image";
 import styles from "./MeetTheTeam.module.scss";
 import { SiLinkedin } from "react-icons/si";
 import { RichText } from "prismic-reactjs";
+//import * as prismicH from '@prismicio/helpers'
+//npm install @prismicio/helpers@beta
 
-//const contentHTML = prismicH.asHTML(prismicDoc.data.example_rich_text)
-
-
-const MeetTheTeam = ({ slice }) => (
-  <SectionWrapper id="meet-the-team" className={styles.section}>
-    <ContentWrapper>
-      {slice.primary.title && (
-        <h2
-          className={styles.title}
-          dangerouslySetInnerHTML={{
-            __html: slice.primary.title[0].text,
-          }}
-        ></h2>
-      )}
-    </ContentWrapper>
-    <ContentWrapper className={styles.team}>
-      {slice?.items?.map((member) => (
-        <div key={member.name}>
-          <Image
-            src={member.profile_picture.url}
-            width={member.profile_picture.dimensions.width}
-            height={member.profile_picture.dimensions.height}
-            alt={member.profile_picture.alt}
-          />
-          <p className={styles.name}>{member.name}</p>
-          <span className={styles.position}>
-            <RichText render={member.company_title} />
-          </span>
-          {member.linkedin_link.url && (
-            <a href={member.linkedin_link.url} target="_blank" title="linkedin">
-              <SiLinkedin className={styles.icon} />
-            </a>
-          )}
-        </div>
-      ))}
-    </ContentWrapper>
-  </SectionWrapper>
-);
+const MeetTheTeam = ({ slice }) => {
+  //const contentHTML = prismicH.asHTML(slice.primary.title)
+  return (
+    <SectionWrapper id="meet-the-team" className={styles.section}>
+      <ContentWrapper>
+        {slice.primary.title && (
+          <h2
+            className={styles.title}
+            dangerouslySetInnerHTML={{
+              __html: slice.primary.title[0].text,
+            }}
+          ></h2>
+        )}
+      </ContentWrapper>
+      <ContentWrapper className={styles.team}>
+        {slice?.items?.map((member) => (
+          <div key={member.name}>
+            <Image
+              src={member.profile_picture.url}
+              width={member.profile_picture.dimensions.width}
+              height={member.profile_picture.dimensions.height}
+              alt={member.profile_picture.alt}
+              quality={100}
+            />
+            <p className={styles.name}>{member.name}</p>
+            <span className={styles.position}>
+              <RichText render={member.company_title} />
+            </span>
+            {member.linkedin_link.url && (
+              <a
+                href={member.linkedin_link.url}
+                target="_blank"
+                title="linkedin"
+              >
+                <SiLinkedin className={styles.icon} />
+              </a>
+            )}
+          </div>
+        ))}
+      </ContentWrapper>
+    </SectionWrapper>
+  );
+};
 
 export default MeetTheTeam;
